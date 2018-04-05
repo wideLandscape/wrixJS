e5ddc69a6749834e346962716ece25fa1912e89e
Adriano Costa <iano76@gmail.com>
2018-04-05 16:44:36 +0200

    add configuration files

---
+++ webpack.common.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'])
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'wrix.js',
+    library: 'wrix',
+    libraryTarget: 'umd'
+  }
+}
