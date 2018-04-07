00fb97deb008f739438765c1d1e78c021d57efc4
Adriano Costa <iano76@gmail.com>
2018-04-07 16:26:55 +0200

    get props from ancestors too

--- src/core/composers/Props.js
+++ src/core/composers/Props.js
@@ -1,7 +1,15 @@
 import * as Configurables from './Configurables'
 import * as Shared from './shared'
 
-export const get = (element = {}) => Object.keys(element).filter(key => isSettable(key, element))
+export const get = (element = {}) => {
+  let props = getProps(element)
+  let prototype = Object.getPrototypeOf(element)
+  do {
+    props = props.concat(getProps(prototype).filter(p => props.indexOf(p) === -1))
+    prototype = Object.getPrototypeOf(prototype)
+  } while (Shared.hasParent(prototype))
+  return props
+}
 
 export const chain = (...args) => {
   return Configurables.chain(...args)
@@ -10,10 +18,13 @@ export const chainAll = (element, configurables = get(element), objectToChain = 
   return Configurables.chainAll(element, configurables, objectToChain)
 }
 
+const getProps = element => Object.keys(element).filter(key => isSettable(key, element))
+
 const isSettable = (key, element) => {
   const descriptor = Object.getOwnPropertyDescriptor(element, key)
-  if (descriptor.configurable) {
+  if (isValidDescriptor(descriptor)) {
     return !Shared.isFunction(element[key])
   }
   return false
 }
+const isValidDescriptor = descriptor => descriptor.configurable && !descriptor.get && !descriptor.set
