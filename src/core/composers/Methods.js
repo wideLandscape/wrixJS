cb391a9d3288c7f62ecb5423f81a0b7facfe068f
Adriano Costa <iano76@gmail.com>
2018-04-07 20:43:25 +0200

    fix methods

--- src/core/composers/Methods.js
+++ src/core/composers/Methods.js
@@ -1,38 +1,39 @@
 import * as Shared from './shared'
 
 export const get = element => {
-  let props = Object.keys(element)
+  let props = availableMethods(element)
   let prototype = Object.getPrototypeOf(element)
   do {
-    props = props.concat(chainableMethods(prototype, element).filter(p => props.indexOf(p) === -1))
+    props = props.concat(chainableMethods(prototype).filter(p => props.indexOf(p) === -1))
     prototype = Object.getPrototypeOf(prototype)
   } while (Shared.hasParent(prototype))
   return props
 }
 
-export const chain = (someFunction, context, objectToChain) => {
+export const chain = (context, functionName, objectToChain) => {
+  const someFunction = context[functionName]
   return function (...args) {
     let value = someFunction.apply(context, args)
     return value === undefined ? objectToChain : value
   }
 }
-export const chainAll = (element, methods = get(element), objectToChain) => {
-  let test = methods.reduce((acc, key) => {
-    acc[key] = chain(element[key], element, acc)
+export const chainAll = (element, methods = get(element), objectToChain = {}) => {
+  return methods.reduce((acc, key) => {
+    acc[key] = chain(element, key, acc)
     return acc
   }, objectToChain)
-  return test
 }
 
-const chainableMethods = (prototype, element) => availableMethods(prototype)
+const chainableMethods = prototype => availableMethods(prototype)
   .filter((p, i, arr) =>
-    Shared.isFunction(element[p]) && // only the methods
-      (i === 0 || p !== arr[i - 1]) // not overriding in fn prototype
+    (i === 0 || p !== arr[i - 1]) // not overriding in fn prototype
   )
 const availableMethods = element =>
   Object.getOwnPropertyNames(element)
+    .concat(Object.keys(element))
     .concat(Object.getOwnPropertySymbols(element).map(s => s.toString()))
     .filter(p =>
+      Shared.isFunction(element[p]) && // only the methods
       p !== 'constructor' && // not the constructor
       !Object.getOwnPropertyDescriptor(element, p).get // nor getters
     )
