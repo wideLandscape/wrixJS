98650d8603fa32af0209853d785c46880a40e4bb
Adriano Costa <iano76@gmail.com>
2018-04-10 14:20:47 +0200

    add wrap

--- src/core/compose.js
+++ src/core/compose.js
@@ -4,6 +4,14 @@ import * as Setters from './composers/Setters'
 import * as Props from './composers/Props'
 import * as Methods from './composers/Methods'
 import * as Shared from './composers/shared'
+
+export const wrap = (element, context, includeFn) => compose({
+  methods: element,
+  props: element,
+  setters: element,
+  getters: element
+}, context, includeFn)
+
 export const compose = (
   { methods = {}, getters = {}, setters = {}, props = {}, configurables = {} } = {},
   context = {},
@@ -15,30 +23,30 @@ export const compose = (
     includeFn = trustAllFn
   }
 
-  let gettersKeys = Getters.get(elements.getters)
-    .filter(includeFn)
-    .filter(Shared.undefinedValuesIn(elements.methods))
-  wrapper = Getters.chainAll(elements.getters, gettersKeys, wrapper)
-
   let configurablesKeys = Configurables.get(elements.configurables)
     .filter(includeFn)
     .filter(Shared.undefinedValuesIn(elements.methods))
   wrapper = Configurables.chainAll(elements.configurables, configurablesKeys, wrapper)
 
-  let propsKeys = Props.get(elements.props)
-    .filter(includeFn)
-    .filter(Shared.undefinedValuesIn(elements.methods))
-  wrapper = Props.chainAll(elements.props, propsKeys, wrapper)
-
-  let settersKeys = Setters.get(elements.setters)
-    .filter(includeFn)
-    .filter(Shared.undefinedValuesIn(elements.methods))
-  wrapper = Setters.chainAll(elements.setters, settersKeys, wrapper)
-
   let methodsKeys = Methods.get(elements.methods)
     .filter(includeFn)
     .filter(Shared.undefinedValuesIn(wrapper))
   wrapper = Methods.chainAll(elements.methods, methodsKeys, wrapper)
+
+  let propsKeys = Props.get(elements.props)
+    .filter(includeFn)
+    .filter(Shared.undefinedValuesIn(wrapper))
+  wrapper = Props.chainAll(elements.props, propsKeys, wrapper)
+
+  let settersKeys = Setters.get(elements.setters)
+    .filter(includeFn)
+    .filter(Shared.undefinedValuesIn(wrapper))
+  wrapper = Setters.chainAll(elements.setters, settersKeys, wrapper)
+
+  let gettersKeys = Getters.get(elements.getters)
+    .filter(includeFn)
+    .filter(Shared.undefinedValuesIn(wrapper))
+  wrapper = Getters.chainAll(elements.getters, gettersKeys, wrapper)
 
   return wrapper
 }
