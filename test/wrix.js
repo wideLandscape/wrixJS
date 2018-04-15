2c198f8f0da7805cffbc7627ee4e6dc61bd6b132
Adriano Costa <iano76@gmail.com>
2018-04-15 13:59:11 +0200

    update test wrix + wrixFactory

--- test/wrix.js
+++ test/wrix.js
@@ -1,31 +1,81 @@
 /* global describe it */
 import { Foo } from './common/class'
-import { createBar } from './common/object'
-import {wrix} from '../src/index'
+import { createBar, createFap } from './common/object'
+import { behaviours } from './common/behaviours'
+import {wrix, wrixFactory} from '../src/index'
 var expect = require('chai').expect
 describe('wrix', function () {
-  it('expect to return itself', function () {
+  it('return itself', function () {
     const foo = new Foo()
-    let localWrix = wrix.wrap(foo).set({ value: 4, x: 4, y: 6 })
-    expect(localWrix).to.be.equal(wrix)
+    let wrixFooRef = wrix().wrap(foo)
+    let anotherWrixFooRef = wrixFooRef.set({ value: 4, x: 4, y: 6 })
+    let wrixBarRef = wrix().wrap(createBar())
+    expect(wrixFooRef.wrix()).to.be.not.equal(wrixBarRef.wrix())
+    expect(wrixFooRef).to.be.equal(anotherWrixFooRef)
   })
 })
-describe('wrix.factory', function () {
-  it('expect to create factories from object', function () {
+describe('WrixFactory', function () {
+  it('creates factories from object', function () {
     const configFactory = {
       key: 'bar',
       factoryFn: createBar
     }
-    let wrappedBar = wrix.factory.create(configFactory).get('bar', { x: 1, y: 2, z: 3 })
+    let wrix = wrixFactory()
+    let wrappedBar = wrix.create(configFactory).get('bar', { y: 2, z: 3 })
     expect(wrappedBar.sum()).to.be.equal(6)
   })
-  it('expect to create factories from class', function () {
+  it('creates factories from object with arguments', function () {
+    const configFactory = {
+      key: 'fap',
+      factoryFn: createFap,
+      factoryArgs: ['x', 'y', 'z']
+    }
+    let wrix = wrixFactory()
+    let wrappedBar = wrix.create(configFactory).get('fap', { x: 1, y: 2, z: 3 })
+    expect(wrappedBar.sum()).to.be.equal(6)
+  })
+  it('creates factories from class', function () {
     const configFactory = {
       key: 'foo',
+      factoryFn: () => new Foo()
+    }
+    let wrappedFoo = wrixFactory(configFactory).get('foo', { x: 1, y: 3 })
+    expect(wrappedFoo.add(4).foo.y).to.be.equal(7)
+  })
+  it('sets custom key context', function () {
+    const configFactory = {
+      key: 'fooCtx',
       factoryFn: () => new Foo(),
       keyContext: '_context'
     }
-    let wrappedFoo = wrix.factory.create(configFactory).get('foo', { x: 1, y: 3 })
-    expect(wrappedFoo.add(4)._context.y).to.be.equal(7)
+    let wrappedFoo = wrixFactory(configFactory)
+      .get('fooCtx', { x: 1, y: 3 })
+      .add(4)
+    expect(wrappedFoo._context.y).to.be.equal(7)
+  })
+  it('adds behaviours', function () {
+    const configFactory = {
+      key: 'fooBehaviour',
+      factoryFn: () => new Foo(),
+      keyContext: '_context',
+      behaviours: behaviours
+    }
+    let wrappedFoo = wrixFactory(configFactory).get('fooBehaviour', { x: 1, y: 3 })
+    expect(wrappedFoo.area(10, 10).x(2).centerToArea()._context.x).to.be.equal(5)
+  })
+  it('persists', function () {
+    let wrappedFoo = wrixFactory().get('fooBehaviour')
+    expect(wrappedFoo).to.be.not.equal(undefined)
+    expect(wrappedFoo).to.be.not.equal(null)
+  })
+  it('gets different instances', function () {
+    let wrappedFoo = wrixFactory().get('fooBehaviour', { x: 1, y: 3 })
+    let wrappedFoo2 = wrixFactory().get('fooBehaviour', { x: 1, y: 3 })
+    wrappedFoo2.x(5)
+    expect(wrappedFoo._context.x).to.be.not.equal(wrappedFoo2._context.x)
+  })
+  it('destroy factory', function () {
+    let wrappedFoo = wrixFactory().destroy('fooBehaviour')
+    expect(wrappedFoo.get('fooBehaviour', { x: 1, y: 3 })).to.be.equal(null)
   })
 })
